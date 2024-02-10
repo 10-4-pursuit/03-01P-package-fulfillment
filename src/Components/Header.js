2bcb431f71b995b24ca16283419006eb8700cb53
EdgarCollado <edgarcollado@pursuit.org>
2024-02-10 16:27:36 -0500

    Added styles to headercss

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../imgs/istockphoto-packages-in-warehouse.jpeg";
+import "./header.css";
+
+const Header = () => {
+    return (
+        <div>
+        <header className="header">
+            <h1>Package Fulfillment React Application</h1>
+            <h3>Mangage Your Packages</h3>
+        </header>
+        </div>
+    );
+};
+
+export default Header;
